c401fc4f013c93ec828a3f02921484230d7b9a20
Julio Alvarez <rokobatichoco@outlook.com>
2023-06-02 00:43:38 -0600

    Datatable

--- src/app/profesores/profesores.component.ts
+++ src/app/profesores/profesores.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profesores',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./profesores.component.css']
 })
 export class ProfesoresComponent {
+  alumnos: any[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    // Hacer la petición HTTP para obtener los datos de los alumnos desde el backend API REST
+    this.http.get<any[]>('http://localhost:3000/alumnos').subscribe(
+      (response) => {
+        this.alumnos = response;
+      },
+      (error) => {
+        console.log('Error al obtener los datos de los alumnos:', error);
+      }
+    );
+  }
 }
